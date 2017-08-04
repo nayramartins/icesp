4e18e81b3bc6db5477bab5862d5758411e4eba68
alinemorelliw <aline.morelli@sprinklr.com>
2017-08-04 14:09:30 -0300

    Add search template

--- search.php
+++ search.php
@@ -1,13 +1,13 @@
 <?php include 'header.php' ?>
 <?php $banner = get_theme_mod('banner_home'); ?>
 <?php $logos = get_theme_mod('regua_logos'); ?>
-<?php if (have_posts()):
-    while ( have_posts() ) : the_post(); ?>
-        <div class="eventos-content">
-            <?php the_title(); ?>
-            <?php the_content(); ?>
-            <?php the_field('data_do_evento'); ?>
-        </div>
+<div class="search-content">
+    <div class="title-content">
+        <h3><?php pll_e( 'Resultados para:'); ?><span><?php echo $_GET['s']; ?></span></h3>
+    </div>
+</div>
+<?php if (have_posts()): ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 <?php endwhile; endif; ?>
 <section class="content-logo">
     <img src="<?php echo $logos; ?>" alt="" />
