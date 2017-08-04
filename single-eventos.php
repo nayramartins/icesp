61098e115d8c6744fca842b6aa962b71a23104c8
alinemorelliw <aline.morelli@sprinklr.com>
2017-08-04 14:03:50 -0300

    Add content to eventos

--- single-eventos.php
+++ single-eventos.php
@@ -1,9 +1,14 @@
 <?php include 'header.php' ?>
 <?php $banner = get_theme_mod('banner_home'); ?>
 <?php $logos = get_theme_mod('regua_logos'); ?>
-<div class="eventos-content">
-
-</div>
+<?php if (have_posts()):
+    while ( have_posts() ) : the_post(); ?>
+        <div class="eventos-content">
+            <?php the_title(); ?>
+            <?php the_content(); ?>
+            <?php the_field('data_do_evento'); ?>
+        </div>
+<?php endwhile; endif; ?>
 <section class="content-logo">
     <img src="<?php echo $logos; ?>" alt="" />
 </section>
