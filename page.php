665a6316766004bc1cf8ebffafe926a77b118613
nayramartins <martinsg.nayra@gmail.com>
2017-08-04 14:10:13 -0300

    ICESP - merge

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php
-/**** Sobre nós template file ****/
+/**** Pages template file ****/
  get_header(); ?>
 
 <div class="breadcrumb">
@@ -12,9 +12,12 @@
     <div class="page--content">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <h1 class="page--title"><?php echo get_field('titulo_da_pagina'); ?></h1>
-            <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
-            <img class="page--featured-image" src="<?php echo $image[0]; ?>" alt="" class="image" />
-            <?php the_content();
+            <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); 
+            if ($image) : ?>
+                <img class="page--featured-image" src="<?php echo $image[0]; ?>" alt="" class="image" />
+            <?php 
+            endif;
+            the_content();
         endwhile; endif; ?>
     </div>
 </section>
