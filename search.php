bb25bd047b4d9c09dc1ddfb29f21e3ef4849a4cc
nayramartins <martinsg.nayra@gmail.com>
2017-08-04 15:35:07 -0300

    ICESP-create search result screen

--- search.php
+++ search.php
@@ -1,25 +1,36 @@
 <?php include 'header.php' ?>
 <?php $banner = get_theme_mod('banner_home'); ?>
 <?php $logos = get_theme_mod('regua_logos'); ?>
-<div class="search-content">
-    <div class="title-content">
-        <h3><?php pll_e( 'Resultados para:'); ?><span><?php echo $_GET['s']; ?></span></h3>
+<div class="breadcrumb">
+    <div class="container">
+        <?php wp_custom_breadcrumbs(); ?>
     </div>
 </div>
-<?php if (have_posts()): ?>
-    <ul class="search-result--content">
-    <?php while ( have_posts() ) : the_post(); ?>
-        <li>
-            <span class="search-text">
-                <a href="<?php the_permalink(); ?>"><p class="title"><?php the_title(); ?></p></a>
-            </span>
-            <p class="text-content"><?php the_excerpt(); ?></p>
-        </li>
-    <?php endwhile; ?>
-    </ul>
-<?php else: ?>
-    <div> <?php pll_e( 'nenhum post' ); ?></div>    
-<?php endif; ?>
+<div class="search page">
+    <div class="page--content">
+        <div class="search-content">
+            <div class="title-content">
+                <h3><?php pll_e( 'Resultados para:'); ?><span><?php echo $_GET['s']; ?></span></h3>
+            </div>
+        </div>
+        <?php if (have_posts()): ?>
+            <ul class="search-result--content">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <li>
+                        <span class="search-text">
+                            <a href="<?php the_permalink(); ?>">
+                                <p class="title"><?php the_title(); ?></p>
+                                <p class="text-content"><?php the_excerpt(); ?></p>
+                            </a>
+                        </span>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+        <?php else: ?>
+            <div> <?php pll_e( 'nenhum post' ); ?></div>
+        <?php endif; ?>
+    </div>
+</div>
 <section class="content-logo">
     <img src="<?php echo $logos; ?>" alt="" />
 </section>
